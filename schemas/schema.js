b3e622acbb9de6c80fc880c8b04075dfcc3c8030
Matt Kelly <bmatthewkelly@gmail.com>
2022-05-17 17:01:40 -0700

    add featured products to home page

--- schemas/schema.js
+++ schemas/schema.js
@@ -6,6 +6,7 @@ import schemaTypes from "all:part:@sanity/base/schema-type";
 
 // Import schemas
 import product from "./product";
+import featured from "./featured";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -16,5 +17,6 @@ export default createSchema({
   types: schemaTypes.concat([
     /* Your types here! */
     product,
+    featured,
   ]),
 });
